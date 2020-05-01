ceba57572989f998b4bf28aa11fb964965c84ffe
亚军 <zyjtoto@163.com>
2020-05-01 18:12:52 +0800

    添加前端控制器

---
+++ src/main/java/com/yajun/jenkins/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.yajun.jenkins.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/index")
+    public Object hello() {
+        return "hello jenkins";
+    }
+}
